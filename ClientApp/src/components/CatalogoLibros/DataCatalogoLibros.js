b19b1330903f69d403903d42a2a944028fd9a278
Alexzp20 <71680855+Alexzp20@users.noreply.github.com>
2023-06-02 00:09:51 -0600

    Catalogo de libros
    Componentes del catalogo de libros
    la pantalla principal
    las cards de la pantalla principal
    el modal de la reserva y préstamo

---
+++ ClientApp/src/components/CatalogoLibros/DataCatalogoLibros.js
@@ -0,0 +1,15 @@
+﻿
+//Archivo para datos de prueba del catalogo
+
+    export const books = [
+        {
+            IdInstlibro: 1,
+            IdLibro: 1,
+            IdIdioma: 1,
+            IdGenero: 1,
+            LogoLibro: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/GRGV335PWFAUBCMQ4AWW2SLGDQ.jpg",
+            AutorLibro: "Michael crichton",
+            TituloLibro: "Jurassic Park",
+            Isbn: 333333333333,
+            CantidadInstanciaLibro: 25
+        }];
---
+++ ClientApp/src/components/CatalogoLibros/ModalCatalogoLibro.js
@@ -0,0 +1,79 @@
+﻿
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Col, Row, Card,CardBody,CardFooter,CardHeader,CardText,CardTitle,ListGroup,ListGroupItem} from 'reactstrap';
+
+
+function ModalCatalogoLibro({ toggle,modal, libro}) {
+
+
+    const idiomas = ['Ingles', 'Español']
+    const categorias = ['acción', 'Suspenso']
+
+    console.log(libro)
+    console.log(libro.LogoLibro)
+
+    return (
+        <div>
+            <Modal size ='xl' isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Descripción de libro </ModalHeader>
+                <ModalBody className="p-2 bg-dark my-1">
+                    <Row >
+                        <Col
+                            xs="6">
+                            <img className="img-fluid shadow-4 mx-auto d-block " src={libro.LogoLibro} alt="logo libro" width="65%"  style={{
+                                height: 400,
+                                alignContent: "center"
+                            }}  ></img>
+                        </Col>
+                        <Col
+                            xs="5">
+                            <br></br>
+                            <br></br>
+                            <Card body>
+                                <CardTitle tag="h5">
+                                    Titulo: {libro.TituloLibro}
+                                </CardTitle>
+                                <ListGroup flush tag = "h6">
+                                    <ListGroupItem>
+                                        Autor: {libro.AutorLibro}
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        Idiomas: {idiomas[libro.IdIdioma]}
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        categorias: {categorias[libro.IdGenero]}
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        ISBN: {libro.Isbn}
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        Copias disponible: {libro.CantidadInstanciaLibro}
+                                    </ListGroupItem>
+                                </ListGroup>
+                            </Card>
+                        </Col>
+                    </Row>
+                </ModalBody>
+                <ModalFooter>
+                    <Button style={{
+                        margin: "5px",
+
+                    }} color="success" outline>
+                        Prestar
+                    </Button>
+                    {' '}
+                    <Button style={{
+                        margin: "5px"
+                    }} color="primary" outline>
+                        Reservar
+                    </Button>
+                    {' '}
+                    <Button color="danger" outline onClick={toggle}>
+                        Cancelar
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    );
+}
+export default ModalCatalogoLibro;
