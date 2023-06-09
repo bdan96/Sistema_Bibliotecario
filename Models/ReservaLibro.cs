9c53a1adbed2901861b39e293eebba9e51ea65e6
bdan96 <74060391+bdan96@users.noreply.github.com>
2023-06-09 15:55:24 -0600

    Cambios en modelo

---
+++ Models/ReservaLibro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sistema_Bibliotecario.Models
+{
+    public partial class ReservaLibro
+    {
+        public ReservaLibro()
+        {
+            Prestamos = new HashSet<Prestamo>();
+        }
+
+        public int Idreservalibro { get; set; }
+        public int? IdInvInst { get; set; }
+        public int? IdPrestamo { get; set; }
+        public int? IdUsuario { get; set; }
+        public DateTime? FechaVencimientoreserva { get; set; }
+
+        public virtual InvInstLibro? IdInvInstNavigation { get; set; }
+        public virtual Prestamo? IdPrestamoNavigation { get; set; }
+        public virtual Usuario? IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Prestamo> Prestamos { get; set; }
+    }
+}
