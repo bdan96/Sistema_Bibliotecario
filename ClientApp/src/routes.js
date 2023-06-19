eccb48b5763dc27a52eb41b97d25a72deb8a6346
Francisco Santamaria <sm19035@ues.edu.sv>
2023-06-19 15:48:58 -0600

    Cambiando el catalogo

--- ClientApp/src/routes.js
+++ ClientApp/src/routes.js
@@ -40,7 +40,7 @@ var routes = [
     },
     {
         path: "/icons",
-        name: "Icons",
+        name: "Catalogo",
         icon: "nc-icon nc-diamond",
         component: <Icons />,
         layout: "/admin",
