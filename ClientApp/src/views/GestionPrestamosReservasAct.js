c9788433d6928c2f41a0ecd09bdc2bfaae050767
Francisco Santamaria <sm19035@ues.edu.sv>
2023-06-20 01:52:40 -0600

    Cambiando la fecha

--- ClientApp/src/views/GestionPrestamosReservasAct.js
+++ ClientApp/src/views/GestionPrestamosReservasAct.js
@@ -55,6 +55,13 @@ function GestionPrestamosReservasActivos() {
         setMostrarModal(!mostrarModal)
     }
 
+    const formatDate = (string) => {
+        let options = { year: 'numeric', month: 'long', day: 'numeric' };
+        let fecha = new Date(string).toLocaleDateString("es-PE", options);
+        let hora = new Date(string).toLocaleTimeString();
+        return fecha + " | " + hora
+    }
+
 
     return (
         <>
@@ -81,7 +88,7 @@ function GestionPrestamosReservasActivos() {
                                             <th>ID</th>
                                             <th>NOMBRE</th>
                                             <th>APELLIDO</th>
-                                            <th>Titulo del libro</th>
+                                            <th>TITULO LIBRO</th>
                                             <th>FECHA PRESTAMO</th>
                                             <th>FECHA VENCIMIENTO PRESTAMO</th>
                                             <th>ACCIONES</th>
@@ -96,8 +103,8 @@ function GestionPrestamosReservasActivos() {
                                                         <td>{item.idUsuarioNavigation.nombres}</td>
                                                         <td>{item.idUsuarioNavigation.apellidos}</td>
                                                         <td>{item.idInvInstNavigation.idInstLibroNavigation.tituloLibro}</td>
-                                                        <td>{item.fechaPrestamo}</td>
-                                                        <td>{item.fechaEntrega}</td>
+                                                        <td>{formatDate(item.fechaPrestamo)}</td>
+                                                        <td>{formatDate(item.fechaEntrega)}</td>
 
 
                                                         <td><Button color="primary">Devolver</Button>{"   "}
@@ -118,7 +125,7 @@ function GestionPrestamosReservasActivos() {
                         <Card>
                             <CardHeader>
                                 <CardTitle tag="h4">Reservas activas</CardTitle>
-                                <Button
+                                {/*<Button
                                     className="btn-round"
                                     color="primary"
                                     type="submit"
@@ -126,7 +133,7 @@ function GestionPrestamosReservasActivos() {
                                     onClick={() => setMostrarModal(!mostrarModal)}
                                 >
                                     Agregar libro
-                                </Button>
+                                </Button>*/}
                             </CardHeader>
                             <CardBody>
                                 <Table responsive>
@@ -136,7 +143,7 @@ function GestionPrestamosReservasActivos() {
                                             <th>NOMBRE</th>
                                             <th>APELLIDO</th>
                                             <th>CORREO</th>
-                                            
+
                                             <th>FECHA VENCIMIENTO</th>
                                             <th>ACCIONES</th>
                                         </tr>
